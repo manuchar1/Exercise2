ac83eb810d5b674e6a581cb4d063a34fa4dbc39c
KATAMADZEG <qatamadzeg@yahoo.com>
2021-09-10 17:41:49 +0400

    Recyclerview

--- app/src/main/java/com/example/exercise2/ui/ShopsAdapter.kt
+++ app/src/main/java/com/example/exercise2/ui/ShopsAdapter.kt
@@ -1,24 +1,17 @@
 package com.example.exercise2.ui
 
 import android.annotation.SuppressLint
-import android.graphics.drawable.Drawable
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.example.exercise2.R
 import com.example.exercise2.databinding.ItemShopBinding
 import com.example.exercise2.model.shops.Shop
+import java.text.ParseException
 import java.text.SimpleDateFormat
 import java.util.*
-import android.R.color
-import android.graphics.Color
-import androidx.core.view.isVisible
-import android.util.Log
-import androidx.core.util.toRange
-import java.text.ParseException
-
 
 class ShopsAdapter : RecyclerView.Adapter<ShopsAdapter.ShopsViewHolder>() {
 
@@ -39,36 +32,43 @@ class ShopsAdapter : RecyclerView.Adapter<ShopsAdapter.ShopsViewHolder>() {
             }
             binding.shopName.text = model.name
 
-            val c: Calendar = Calendar.getInstance()
-            val sdf = SimpleDateFormat("kk:mm:ss")
-          //  val currentTime: String = sdf.format(c.time)
-            val currentTime = "09:00:00"
-
-
-
             for (time in model.workingHours) {
 
-                val workingHours = time.from..time.to
-                val working = time.working
+                try {
+                    val startingTime = time.from
+                    val finishingTime = time.to
+                    val formatShort = SimpleDateFormat("HH:MM")
+                    val openTime = formatShort.format(formatShort.parse(startingTime))
+                    val closeTime = formatShort.format(formatShort.parse(finishingTime))
+                    // val workingHours = openTime..closeTime
+                    val isWorking = time.working
 
-                if (working && !workingHours.contains(currentTime)) {
+                    fun currentTimeBetweenGivenString(s1: String, s2: String): Boolean {
+                        val simpleDateFormat = SimpleDateFormat("HH:MM")
+                        val date1: Date = simpleDateFormat.parse(s1)
+                        val date2: Date = simpleDateFormat.parse(s2)
+                        val d = Date()
+                        val s3 = simpleDateFormat.format(d)
+                        val date3 = simpleDateFormat.parse(s3)
 
-                    binding.apply {
-                        ivBackground.setColorFilter(Color.argb(155, 0, 0, 0))
-                        ivMoonIcon.isVisible = true
-                        tvWorkingHours.isVisible = true
-                        btnOrderPlaning.isVisible = true
-                        tvWorkingHours.text = "${time.day}, ${time.from} - ${time.to}"
-
+                        return date3 >= date1 && date3 <= date2
                     }
+                    if (isWorking && currentTimeBetweenGivenString(openTime, closeTime)) {
 
+                     //   binding.tvCurrentTime.text = "######"
 
-                } else {
-                    binding.tvCurrentTime.text = "######"
-
+                        binding.apply {
+                            ivBackground.setColorFilter(Color.argb(155, 0, 0, 0))
+                            ivMoonIcon.isVisible = true
+                            tvWorkingHours.isVisible = true
+                            btnOrderPlaning.isVisible = true
+                            tvWorkingHours.text = "${time.day}, $openTime - $closeTime"
+                        }
+                    }
+                    // binding.tvDeliveryStatus.text = workingHours.toString()
+                } catch (e: ParseException) {
+                    e.printStackTrace()
                 }
-                binding.tvDeliveryStatus.text = workingHours.toString()
-               // binding.tvCurrentTime.text = currentTime
             }
         }
     }
